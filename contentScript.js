63b8c241fc4d2e2cddcacb7055235842807687df
loafyyy <jackiepeng12@gmail.com>
2019-07-24 23:42:47 -0400

    initial commit

---
+++ contentScript.js
@@ -0,0 +1,7 @@
+document.onkeypress = function(e) {
+    e = e || window.event;
+    var charCode = (typeof e.which == "number") ? e.which : e.keyCode;
+    if (charCode) {
+        alert("Character typed: " + String.fromCharCode(charCode));
+    }
+};
